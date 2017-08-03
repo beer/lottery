679c8a1f45e598e3ebf643cd4d2d8da92b6fc6cb
beer <beer@eatgo.com>
2017-08-03 19:15:11 +0800

    add deployer
    Signed-off-by: beer <beer@eatgo.com>

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Configuration
+
+set('repository', 'git@github.com:beer/lottery.git');
+//set('git_tty', true); // [Optional] Allocate tty for git on first deployment
+set('shared_files', ['config/database.json']);
+set('shared_dirs', []);
+set('writable_dirs', []);
+set('default_stage', 'prod');
+
+
+// Hosts
+
+host('lottery.speculator.im')
+    ->user('deployer')
+    ->stage('prod')
+    ->set('deploy_path', '/srv/www/lottery');
+    
+desc('Deploy your project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    //'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
